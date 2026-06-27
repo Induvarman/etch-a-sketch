f19bae28d370e0e9038051c7262f236f85d89bd4
Induvarman <induvarman8@gmail.com>
2026-06-27 15:56:18 +0530

    Completed the core hovering logic with mouseover with user input from form.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const gridContainer = document.querySelector(".grid");
-const order = document.querySelector("#order");
+const form = document.querySelector("form");
 
 function createGrid(order) {
   for (let i = 0; i < order * order; i++) {
@@ -15,4 +15,22 @@ gridContainer.addEventListener("mouseover", e => {
   if (e.target.classList.contains("cell"))
     e.target.classList.add("add-color");
 })
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const orderObj = e.target.elements.order.value;
+  order = Number(orderObj);
+  
+  gridContainer.innerHTML = "";
+
+  createGrid(order);
+
+  gridContainer.style.setProperty("--order", order);
+
+  gridContainer.addEventListener("mouseover", e => {
+  if (e.target.classList.contains("cell"))
+    e.target.classList.add("add-color");
+});
+
+});
 
